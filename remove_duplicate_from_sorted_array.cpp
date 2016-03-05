cf71f286b086ccf527ad3334b4750086688a4367
Yancy Xerxes <704597765@qq.com>
2016-03-05 21:58:58 +0800

    Create remove_duplicate_from_sorted_array.cpp

---
+++ remove_duplicate_from_sorted_array.cpp
@@ -0,0 +1,36 @@
+class Solution {
+public:
+    int removeDuplicates(vector<int>& nums) {
+        if (nums.size() == 0)
+            return 0;
+        if (nums.size() == 1)
+            return 1;
+        
+        int i=1, size=nums.size();
+        
+        while (i!=size && nums[i]!=nums[i-1])  // i points to the first repeated element
+            i++;
+        if (i == size)              // all elements are different
+            return size;
+        
+        int j=i;
+        while (j!=size && nums[j]==nums[i])    // j points to the next different element
+            j++;
+        if (j == size)
+            return i;
+        
+        while (true){
+            int tempj = nums[j];
+            swap(nums[i], nums[j]);
+            j++;                        // j points to next position
+            if (j == size)
+                break;
+            while (j!=size && nums[j]==tempj)    
+                j++;                    // j points to next different element
+            if (j == size)
+                break;
+            i++;
+        }
+        return i+1;
+    }
+};
