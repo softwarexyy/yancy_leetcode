63a6de797c29dbfbf9baee6aea596fe8eeec8f78
Yancy Xerxes <704597765@qq.com>
2016-03-15 19:57:19 +0800

    Create combination_sum.cpp

---
+++ combination_sum.cpp
@@ -0,0 +1,37 @@
+class Solution {
+public:
+    void backtracking(vector<vector<int>>& res, vector<int>& candidates, vector<int>& tmp, int target, int index){
+        int sum = 0;
+        for (int i=0; i<tmp.size(); i++)
+            sum += tmp[i];
+        
+        if (sum == target){
+            res.push_back(tmp);
+        }
+        if (sum < target){
+            for (int i=index; i<candidates.size(); i++){    //从index开始添加元素
+                tmp.push_back(candidates[i]);
+                backtracking(res, candidates, tmp, target, i);
+                tmp.pop_back();
+            }
+        }
+    }
+    
+    vector<vector<int>> combinationSum(vector<int>& candidates, int target) {
+        sort(candidates.begin(), candidates.end());
+        vector<vector<int>> res;
+        vector<int> tmp;
+        backtracking(res, candidates, tmp, target, 0);
+        return res;
+    }
+};
+
+
+
+
+
+//方法：
+//  逐个添加元素，验证已选元素的和sum是否有sum==target
+//  如果sum<target 说明还可以添加元素(用递归添加)
+//  如果sum==target 说明当前已添加的元素符合条件，纳入结果res中
+//  注意：先给candidates排序
