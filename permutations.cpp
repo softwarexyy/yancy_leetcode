e1414873706ea8b0f662d65019220c13b0a9c3c2
Yancy Xerxes <704597765@qq.com>
2016-03-23 01:27:21 +0800

    Create permutations.cpp

---
+++ permutations.cpp
@@ -0,0 +1,38 @@
+class Solution {
+public:
+    void mypermute(vector<int> nums, vector<vector<int>>& res, vector<int> tmpres){    //tmpres是上一次遗留的头部,res是最终结果
+        if (nums.empty())
+            res.push_back(tmpres);
+
+        for (int i=0; i<nums.size(); i++){
+            vector<int> tmpres_tmp = tmpres;    //为了防止几次循环改变tmpres和nums(在选取不同的头部元素时，tmpres和nums是不允许被改变的)
+            vector<int> numstmp = nums;
+            
+            tmpres_tmp.push_back(nums[i]);
+            numstmp.erase(numstmp.begin() + i);   //delete the element to go on next recursion
+            mypermute(numstmp, res, tmpres_tmp);
+        }
+    }
+    
+    vector<vector<int>> permute(vector<int>& nums) {
+        sort(nums.begin(), nums.end());
+        vector<vector<int>> res;
+        vector<int> tmpres;
+        mypermute(nums, res, tmpres);
+        return res;
+    }
+};
+
+
+
+
+
+
+
+//方法：
+//  先固定一个数，再对剩余的数进行排列，这样就形成了递归结构
+//  permutation(int n){
+//      1、选取一个数作为头部；tmp.push_back(选中的数)
+//      2、踢掉第1步中的数，再进行tmp.insert(tmp.begin(), permutation(n-1));
+//  }
+//  每次递归都改变最终结果res,这样等到递归结束时就得到正确的res
