a29fff53362a7a35e4220ea1b8fa7d45895d3b1d
Yancy Xerxes <704597765@qq.com>
2016-03-23 11:58:48 +0800

    Create permutations_II.cpp

---
+++ permutations_II.cpp
@@ -0,0 +1,28 @@
+class Solution {
+public:
+    void mypermute(vector<int>& nums, vector<vector<int>>& res, vector<int>& prefix){
+        if (nums.empty())
+            res.push_back(prefix);
+        
+        int previous;
+        for (int i=0; i<nums.size(); i++){
+            if (i!=0 && nums[i]==previous)          //如果选取的头部与上次一样，则略过，避免重复排列
+                continue;
+               
+            vector<int> nums_tmp = nums;            //prevent to modify nums and prefix
+            vector<int> prefix_tmp = prefix;
+                
+            prefix_tmp.push_back(nums[i]);
+            nums_tmp.erase(nums_tmp.begin() + i);   //delete the head element
+            mypermute(nums_tmp, res, prefix_tmp);
+            previous = nums[i];
+        }
+    }
+    vector<vector<int>> permuteUnique(vector<int>& nums) {
+        sort(nums.begin(), nums.end());
+        vector<vector<int>> res;
+        vector<int> tmpres;
+        mypermute(nums, res, tmpres);
+        return res;
+    }
+}
